058d69739754487453157342fe18047071d0d49d
Denis Prak <denisprak@MacBook-Air-de-Denis.local>
2018-08-10 22:25:22 +0200

    chang edatabase provider

--- src/common/services/database.providers.ts
+++ src/common/services/database.providers.ts
@@ -3,12 +3,10 @@ import { createConnection } from 'typeorm';
 export const databaseProviders = [{
     provide: 'DbConnectionToken',
     useFactory: async () => await createConnection({
-        type: 'mysql',
-        name: 'etna_rest',
-        database: 'etna_rest',
-        port: 3306,
-        username: 'root',
-        password: 'root',
+        type: 'postgres',
+        name: 'default',
+        // database: 'das7ul1a8vpo1t',
+        url: 'postgres://mlfresfpzomtcq:5def83e28f25d3c24f71fb8aa0a3b5ef43c59030fe702429d3c085b752cfcde5@ec2-54-235-94-36.compute-1.amazonaws.com:5432/das7ul1a8vpo1t',
         // synchronize: true,
         logging: true,
         entities: [
