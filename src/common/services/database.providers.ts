e56e325cc9c5bd24ab832325e77706707b6af324
Denis PRAK <denis.prak@sncf.fr>
2018-08-12 16:46:28 +0200

    post on heroku

--- src/common/services/database.providers.ts
+++ src/common/services/database.providers.ts
@@ -5,9 +5,7 @@ export const databaseProviders = [{
     useFactory: async () => await createConnection({
         type: 'postgres',
         name: 'default',
-        // database: 'das7ul1a8vpo1t',
-        url: 'postgres://postgres:postgres@localhost/das7ul1a8vpo1t',
-        // synchronize: true,
+        url: 'postgres://mlfresfpzomtcq:5def83e28f25d3c24f71fb8aa0a3b5ef43c59030fe702429d3c085b752cfcde5@ec2-54-235-94-36.compute-1.amazonaws.com:5432/das7ul1a8vpo1t',
         logging: true,
         entities: [
             __dirname + '/../models/**/*.entity{.ts,.js}'
